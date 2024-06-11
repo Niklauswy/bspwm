ff09815e883e38a87522309241f0633d78e3dc53
Nicolas <niklaustein@gmail.com>
2024-06-11 00:31:47 -0700

    Yei

--- instalar.sh
+++ instalar.sh
@@ -148,16 +148,6 @@ function APPS()
             && sudo apt update \
             && sudo apt install gh -y
 			
-			#Brave
-			sudo apt install curl
-			sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-			echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/>
-			sudo apt update
-			sudo apt install brave-browser
-
-			clear &&
-			echo "#-----------------------Apps complementarias instaladas------------------------#"
 			sleep 2s
 	}
 
